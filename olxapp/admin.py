1c7460f6aba247e7c7e0ce885b6888d1343cb539
YashnarbekS <122490631+Programmeryashnar@users.noreply.github.com>
2023-12-01 22:23:08 +0500

    olxproject

--- olxapp/admin.py
+++ olxapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from olxapp.models import Category, UserProfile, Product, Transaction
+
+admin.site.register(Category)
+admin.site.register(UserProfile)
+admin.site.register(Product)
+admin.site.register(Transaction)
+
 
 # Register your models here.
